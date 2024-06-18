dbed97be851958c66f144d64ff59cab82fb3655a
sina aligholizadeh <46600297+sina158@users.noreply.github.com>
2024-06-18 18:00:18 -0400

    Update scripts.js
    optimization

--- scripts.js
+++ scripts.js
@@ -220,53 +220,6 @@ threeJsScript.onload = () => {
         }
     }
 
-    class Environment {
-        constructor(font, particle) {
-            this.font = font;
-            this.particle = particle;
-            this.container = document.querySelector('#magic');
-            this.scene = new THREE.Scene();
-            this.createCamera();
-            this.createRenderer();
-            this.setup();
-            this.bindEvents();
-        }
-
-        bindEvents() {
-            window.addEventListener('resize', this.onWindowResize.bind(this));
-        }
-
-        setup() {
-            this.createParticles = new CreateParticles(this.scene, this.font, this.particle, this.camera, this.renderer);
-        }
-
-        render() {
-            this.createParticles.render();
-            this.renderer.render(this.scene, this.camera);
-        }
-
-        createCamera() {
-            this.camera = new THREE.PerspectiveCamera(65, this.container.clientWidth / this.container.clientHeight, 1, 10000);
-            this.camera.position.set(0, 0, 100);
-        }
-
-        createRenderer() {
-            this.renderer = new THREE.WebGLRenderer({ alpha: true }); // Enable alpha for transparency
-            this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
-            this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-            this.renderer.outputEncoding = THREE.sRGBEncoding;
-            this.renderer.setClearColor(0x000000, 0); // Set background to transparent
-            this.container.appendChild(this.renderer.domElement);
-            this.renderer.setAnimationLoop(() => { this.render() });
-        }
-
-        onWindowResize() {
-            this.camera.aspect = this.container.clientWidth / this.container.clientHeight;
-            this.camera.updateProjectionMatrix();
-            this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
-        }
-    }
-
     const preload = () => {
         let manager = new THREE.LoadingManager();
         manager.onLoad = function () {
